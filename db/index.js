2b8c0fc039a135d7c8fa66004eaef890a8435014
杨铭 <yangming_pro@163.com>
2023-06-14 14:40:03 +0800

    feat: 修改数据库账号密码

--- db/index.js
+++ db/index.js
@@ -3,11 +3,11 @@ const mysql = require("mysql");
 const domain = "124.220.133.135"; // 阿里云服务器
 // 建立一个连接池
 const db = mysql.createPool({
-    host: domain, // 数据库的IP地址(本地的或者是云服务器的都可以)
-    user: "admin",
-    password: "root",
-    database: "server", //指定要操作哪个数据库
-    port: 3306,
+	host: domain, // 数据库的IP地址(本地的或者是云服务器的都可以)
+	user: "server",
+	password: "KriZCRiXdbCePaCY",
+	database: "server", //指定要操作哪个数据库
+	port: 3306,
 });
 
 // 将文件暴露出去
